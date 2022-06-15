f5e7c3ae0611c07c1d098008a6269e3b30501799
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-06-15 20:24:00 +0300

    Updated encryption scrypt to emit newfiles instead of patch for better merging

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -4,21 +4,22 @@ PASS="{{command_output "secret-tool lookup dotter password"}}"
 
 for file in $PWD/.dotter/private/*.asc
 do
-	# If there are any patch files, abort
-	if test -f "${file%.*}.patch"
+	# If there are any new files, abort
+	if test -f "${file%.*}.new"
 	then
-		echo "$(basename -- "${file%.*}").patch exists."
+		echo "$(basename -- "${file%.*}").new exists."
 		echo "resolve conflicts before deploy"
 		exit 1
 	fi
 
 	# decrypt and diff with the existing file
-	gpg --armor --batch --yes --passphrase $PASS --decrypt  $file | diff -N "${file%.*}" - > "${file%.*}.patch"
+	gpg --armor --batch --yes --passphrase $PASS --decrypt  $file > "${file%.*}.new"
+
+	diff -N "${file%.*}" "${file%.*}.new"
 	if [ $? -eq 0 ]
 	then
-		# if the file wasn't changed, there will be an empty patch file
-		# delete it
-		rm "${file%.*}.patch"
+		# if the file wasn't changed, remove .new file
+		rm "${file%.*}.new"
 	else
 		# abort in case of any errors in diff
 		if [ $? -eq 2 ]
