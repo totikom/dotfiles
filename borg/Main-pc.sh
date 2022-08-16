d82af31d16b3428c828add3bcd1d1b141f4a0b7b
Eugene Lomov <eugene.v.lomov@gmail.com>
2022-08-16 15:43:43 +0300

    Added full-disk backup scripts

---
+++ borg/Main-pc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# The backup partition is mounted there
+
+MOUNTPOINT="/mnt/Backup"
+
+# This is the location of the Borg repository
+TARGET=$MOUNTPOINT/Main-pc
+
+#Passphrase
+export BORG_PASSPHRASE="{{main_pc_offline_pass}}"
+
+PRUNE_OPTS="-d 7 -w 5 -m 6 -y 2"
+
+DATE=$(date)
+
+BACKUPS="/dev/sda /mnt/Media/Books /mnt/Media/cloud_archive /mnt/Media/Pictures"
+SPACED=""
+
+# Options for borg create
+BORG_OPTS="--stats --progress --read-special"
+
+# Log Borg version
+borg --version
+
+if [ -n "$1"  ]; then
+	# Prune
+	echo 'Prunning:'
+	borg prune -v --list --stats $PRUNE_OPTS $TARGET
+  else
+	#
+	# Create backups
+	#
+
+	echo "Starting backup for $DATE"
+
+	borg create $BORG_OPTS \
+		$TARGET::{now} \
+		$BACKUPS \
+		--exclude-from /home/eugene/.borg/exclusions
+
+	echo "Completed backup for $DATE"
+
+	echo 'Backups to prune:'
+	borg prune -v --list --dry-run $PRUNE_OPTS $TARGET
+fi
+
+# Just to be completely paranoid
+sync
