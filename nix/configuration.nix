5577b146324c006f78b4892333ae06b7388a1aa4
Eugene Lomov <eugene.lomov@protonmail.com>
2025-05-25 01:18:31 +0300

    Format configs

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -2,129 +2,135 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports = [ # Include the results of the hardware scan.
-  ./hardware-configuration.nix
-  <home-manager/nixos>
-  ./home.nix
-];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+    ./home.nix
+  ];
 
-# Use the systemd-boot EFI boot loader.
-boot.loader.systemd-boot.enable = true;
-boot.loader.efi.canTouchEfiVariables = true;
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-networking.hostName = "ThinkPadT490s"; # Define your hostname.
-# Pick only one of the below networking options.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "ThinkPadT490s"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
-# Set your time zone.
-time.timeZone = "Europe/Moscow";
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-# Select internationalisation properties.
-i18n.defaultLocale = "en_US.UTF-8";
-# console = {
-#   font = "Lat2-Terminus16";
-#   keyMap = "us";
-#   useXkbConfig = true; # use xkb.options in tty.
-# };
-fonts.packages = with pkgs; [
-  nerd-fonts.hack
-  nerd-fonts.fira-code
-  font-awesome
-];
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+  fonts.packages = with pkgs; [
+    nerd-fonts.hack
+    nerd-fonts.fira-code
+    font-awesome
+  ];
 
-# List services that you want to enable:
-services = {
-# Enable the X11 windowing system.
-xserver = {
-  enable = true;
-  desktopManager.xterm.enable = false;
-  windowManager.i3 = {
-    enable = true;
+  # List services that you want to enable:
+  services = {
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      desktopManager.xterm.enable = false;
+      windowManager.i3 = {
+        enable = true;
+      };
+    };
+    displayManager.defaultSession = "none+i3";
+    pcscd.enable = true;
+    # Enable sound.
+    # hardware.pulseaudio.enable = true;
+    # OR
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+    # Configure keymap in X11
+    #services.xserver.xkb.layout = "us";
+    # services.xserver.xkb.options = "eurosign:e,caps:escape";
   };
-};
-displayManager.defaultSession = "none+i3";
-pcscd.enable = true;
-# Enable sound.
-# hardware.pulseaudio.enable = true;
-# OR
-pipewire = {
-  enable = true;
-  pulse.enable = true;
-};
-# Enable CUPS to print documents.
-printing.enable = true;
 
-# Enable touchpad support (enabled default in most desktopManager).
-libinput.enable = true;
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eugene = {
+    shell = pkgs.zsh;
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
 
-# Enable the OpenSSH daemon.
-openssh.enable = true;
-# Configure keymap in X11
-#services.xserver.xkb.layout = "us";
-# services.xserver.xkb.options = "eurosign:e,caps:escape";
-};
+  # programs.firefox.enable = true;
+  programs.zsh.enable = true;
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-users.users.eugene = {
-  shell = pkgs.zsh;
-  isNormalUser = true;
-  extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-};
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    htop
+  ];
 
-# programs.firefox.enable = true;
-programs.zsh.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-environment.systemPackages = with pkgs; [
-  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  wget
-  htop
-];
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-programs.mtr.enable = true;
-programs.gnupg.agent = {
-  enable = true;
-  enableSSHSupport = true;
-};
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
 
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-networking.firewall.enable = false;
-
-# Copy the NixOS configuration file and link it from the resulting system
-# (/run/current-system/configuration.nix). This is useful in case you
-# accidentally delete configuration.nix.
-system.copySystemConfiguration = true;
-
-# This option defines the first version of NixOS you have installed on this particular machine,
-# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-#
-# Most users should NEVER change this value after the initial install, for any reason,
-# even if you've upgraded your system to a new NixOS release.
-#
-# This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-# so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-# to actually do that.
-#
-# This value being lower than the current NixOS release does NOT mean your system is
-# out of date, out of support, or vulnerable.
-#
-# Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-# and migrated your data accordingly.
-#
-# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-system.stateVersion = "24.11"; # Did you read the comment?
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
--- nix/home.nix
+++ nix/home.nix
@@ -1,204 +1,211 @@
 { config, pkgs, ... }:
 {
   home-manager.users.eugene = {
-# Home Manager needs a bit of information about you and the paths it should
-# manage.
-home.username = "eugene";
-home.homeDirectory = "/home/eugene";
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = "eugene";
+    home.homeDirectory = "/home/eugene";
 
-# This value determines the Home Manager release that your configuration is
-# compatible with. This helps avoid breakage when a new Home Manager release
-# introduces backwards incompatible changes.
-#
-# You should not change this value, even if you update Home Manager. If you do
-# want to update the value, then make sure to first check the Home Manager
-# release notes.
-home.stateVersion = "25.05"; # Please read the comment before changing.
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "25.05"; # Please read the comment before changing.
 
-# The home.packages option allows you to install Nix packages into your
-# environment.
-home.packages = with pkgs; [
-  htop
-  neofetch
-  firefox
-  alacritty
-  eza
-  dotter
-  ncdu
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    home.packages = with pkgs; [
+      alacritty
+      dotter
+      eza
+      firefox
+      htop
+      ncdu
+      neofetch
+      nixfmt-rfc-style
 
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-# # It is sometimes useful to fine-tune packages, for example, by applying
-# # overrides. You can do that directly here, just don't forget the
-# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-# # fonts?
-# (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      # (pkgs.writeShellScriptBin "my-hello" ''
+      #   echo "Hello, ${config.home.username}!"
+      # '')
+    ];
 
-# # You can also create simple shell scripts directly inside your
-# # configuration. For example, this adds a command 'my-hello' to your
-# # environment:
-# (pkgs.writeShellScriptBin "my-hello" ''
-#   echo "Hello, ${config.home.username}!"
-# '')
-                        ];
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
 
-# Home Manager is pretty good at managing dotfiles. The primary way to manage
-# plain files is through 'home.file'.
-home.file = {
-# # Building this configuration will create a copy of 'dotfiles/screenrc' in
-# # the Nix store. Activating the configuration will then make '~/.screenrc' a
-# # symlink to the Nix store copy.
-# ".screenrc".source = dotfiles/screenrc;
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
 
-# # You can also set the file content immediately.
-# ".gradle/gradle.properties".text = ''
-#   org.gradle.console=verbose
-#   org.gradle.daemon.idletimeout=3600000
-# '';
-                };
-
-                home.sessionVariables = {
-                  SHELL = "${pkgs.zsh}/bin/zsh";
-                };
-                programs = {
-                  zoxide = {enable = true;};
-                  bat = {enable = true;};
-                  git = {
-                    enable = true;
-                    delta.enable = true;
-                    userName = "Eugene Lomov";
-                    userEmail = "eugene.lomov@protonmail.com";
-                    signing = {
-                      key = "3197B6B3AE53574B";
-                      signByDefault = true;
-                    };
-                    extraConfig.push.autoSetupRemote = true;
-                  };
-                  gitui = { enable = true; };
-                  vim = {
-                    enable = true;
-                    defaultEditor = true;
-                  };
-                  zsh = {
-                    enable = true;
-                    enableCompletion = true;
-                    autosuggestion.enable = true;
-                    syntaxHighlighting.enable = true;
-                  };
-                  zellij = {
-                    enable = true;
-                    enableZshIntegration = true;
-                    attachExistingSession = true;
-                  };
-                  starship = {
-                    enable = true;
-                    enableZshIntegration = true;
-                    settings = {
-                      add_newline = true;
-                      format = "$all";
-                      scan_timeout = 100;
-                    };
-                  };
-                  i3status-rust = {
-                    enable = true;
-                    bars = {
-                      default = {
-                        blocks = [
-                          {
-                            block = "music";
-                            format = " $icon {$combo.str(max_w:20) $play $next |}";
-                          }
-                          {
-                            block = "memory";
-                            interval = 5;
-                            warning_mem = 80;
-                            critical_mem = 95;
-                            warning_swap = 80;
-                            critical_swap = 95;
-                            format = " $icon $mem_total_used_percents.eng(w:2) ";
-                            format_alt = " $icon_swap $swap_free.eng(w:3,u:B,p:M)/$swap_total.eng(w:3,u:B,p:M)($swap_used_percents.eng(w:2)) ";
-                          }
-                          {
-                            block = "cpu";
-                            interval = 1;
-                          }
-                          { block = "sound"; }
-                          {
-                            block = "net";
-                            format = " $icon {$signal_strength $ssid |}";
-                            format_alt = " $icon ^icon_net_down $speed_down.eng(prefix:K) ^icon_net_up $speed_up.eng(prefix:K) ";
-                          }
-                          {
-                            block = "vpn";
-                            driver = "mullvad";
-                            interval = 10;
-                            format_connected = "$icon ";
-                            format_disconnected = " $icon ";
-                            state_connected = "good";
-                            state_disconnected = "critical";
-                          }
-                          {
-                            block = "battery";
-                            interval = 10;
-                            format = " $icon $percentage $time ";
-                            full_threshold = 99;
-                            info = 50;
-                            warning = 30;
-                            critical = 10;
-                            empty_threshold = 1;
-                          }
-                          {
-                            block = "time";
-                            interval = 5;
-                            format = " $icon $timestamp.datetime(f:'%a %d/%m %R') ";
-                          }
-                        ];
-                        icons = "awesome5";
-                        theme = "solarized-dark";
-                      };
-                    };
-                  };
-                  rofi = {
-                    enable = true;
-                    terminal = "${pkgs.alacritty}/bin/alacritty";
-                  };
-                };
-
-
-# Home Manager can also manage your environment variables through
-# 'home.sessionVariables'. These will be explicitly sourced when using a
-# shell provided by Home Manager. If you don't want to manage your shell
-# through Home Manager then you have to manually source 'hm-session-vars.sh'
-# located at either
-#
-#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-#
-# or
-#
-#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-#
-# or
-#
-#  /etc/profiles/per-user/eugene/etc/profile.d/hm-session-vars.sh
-#
-home.sessionVariables = {
-# EDITOR = "emacs";
-                };
-                xsession.windowManager.i3 = {
-                  enable = true;
-                  config = {
-                    modifier = "Mod4";
-                    terminal = "alacritty";
-                    bars = [
-                      {
-                        position = "top";
-                        statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
-                      }
-                    ];
-                  };
-                };
-
-# Let Home Manager install and manage itself.
-programs.home-manager.enable = true;
+    home.sessionVariables = {
+      SHELL = "${pkgs.zsh}/bin/zsh";
+    };
+    programs = {
+      zoxide = {
+        enable = true;
+      };
+      bat = {
+        enable = true;
+      };
+      git = {
+        enable = true;
+        delta.enable = true;
+        userName = "Eugene Lomov";
+        userEmail = "eugene.lomov@protonmail.com";
+        ignores = [
+          "*.swp"
+        ];
+        signing = {
+          key = "3197B6B3AE53574B";
+          signByDefault = true;
         };
-      }
+        extraConfig.push.autoSetupRemote = true;
+      };
+      gitui = {
+        enable = true;
+      };
+      vim = {
+        enable = true;
+        defaultEditor = true;
+      };
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+      };
+      zellij = {
+        enable = true;
+        enableZshIntegration = true;
+        attachExistingSession = true;
+      };
+      starship = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          add_newline = true;
+          format = "$all";
+          scan_timeout = 100;
+        };
+      };
+      i3status-rust = {
+        enable = true;
+        bars = {
+          default = {
+            blocks = [
+              {
+                block = "music";
+                format = " $icon {$combo.str(max_w:20) $play $next |}";
+              }
+              {
+                block = "memory";
+                interval = 5;
+                warning_mem = 80;
+                critical_mem = 95;
+                warning_swap = 80;
+                critical_swap = 95;
+                format = " $icon $mem_total_used_percents.eng(w:2) ";
+                format_alt = " $icon_swap $swap_free.eng(w:3,u:B,p:M)/$swap_total.eng(w:3,u:B,p:M)($swap_used_percents.eng(w:2)) ";
+              }
+              {
+                block = "cpu";
+                interval = 1;
+              }
+              { block = "sound"; }
+              {
+                block = "net";
+                format = " $icon {$signal_strength $ssid |}";
+                format_alt = " $icon ^icon_net_down $speed_down.eng(prefix:K) ^icon_net_up $speed_up.eng(prefix:K) ";
+              }
+              {
+                block = "vpn";
+                driver = "mullvad";
+                interval = 10;
+                format_connected = "$icon ";
+                format_disconnected = " $icon ";
+                state_connected = "good";
+                state_disconnected = "critical";
+              }
+              {
+                block = "battery";
+                interval = 10;
+                format = " $icon $percentage $time ";
+                full_threshold = 99;
+                info = 50;
+                warning = 30;
+                critical = 10;
+                empty_threshold = 1;
+              }
+              {
+                block = "time";
+                interval = 5;
+                format = " $icon $timestamp.datetime(f:'%a %d/%m %R') ";
+              }
+            ];
+            icons = "awesome5";
+            theme = "solarized-dark";
+          };
+        };
+      };
+      rofi = {
+        enable = true;
+        terminal = "${pkgs.alacritty}/bin/alacritty";
+      };
+    };
 
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/eugene/etc/profile.d/hm-session-vars.sh
+    #
+    home.sessionVariables = {
+      # EDITOR = "emacs";
+    };
+    xsession.windowManager.i3 = {
+      enable = true;
+      config = {
+        modifier = "Mod4";
+        terminal = "alacritty";
+        bars = [
+          {
+            position = "top";
+            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
+          }
+        ];
+      };
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+  };
+}
