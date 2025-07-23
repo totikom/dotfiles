60cd96a763310334999305b59c228dafd1b731b7
Eugene Lomov <eugene.lomov@protonmail.com>
2025-07-23 23:33:49 +0300

    home.nix formatting

--- nix/home.nix
+++ nix/home.nix
@@ -11,68 +11,93 @@ in
   home-manager.users.eugene = {
     # Home Manager needs a bit of information about you and the paths it should
     # manage.
-    home.username = "eugene";
-    home.homeDirectory = "/home/eugene";
+    home = {
+      username = "eugene";
+      homeDirectory = "/home/eugene";
 
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    home.stateVersion = "25.05"; # Please read the comment before changing.
+      # This value determines the Home Manager release that your configuration is
+      # compatible with. This helps avoid breakage when a new Home Manager release
+      # introduces backwards incompatible changes.
+      #
+      # You should not change this value, even if you update Home Manager. If you do
+      # want to update the value, then make sure to first check the Home Manager
+      # release notes.
+      stateVersion = "25.05"; # Please read the comment before changing.
 
-    # The home.packages option allows you to install Nix packages into your
-    # environment.
-    home.packages = with pkgs; [
-      dotter
-      eza
-      firefox
-      htop
-      ncdu
-      neofetch
-      nixfmt-rfc-style
-      trashy
-      xorg.xbacklight
+      # The home.packages option allows you to install Nix packages into your
+      # environment.
+      packages = with pkgs; [
+        dotter
+        eza
+        firefox
+        htop
+        ncdu
+        neofetch
+        nixfmt-rfc-style
+        trashy
+        xorg.xbacklight
 
-      # # It is sometimes useful to fine-tune packages, for example, by applying
-      # # overrides. You can do that directly here, just don't forget the
-      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # # fonts?
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+        # # It is sometimes useful to fine-tune packages, for example, by applying
+        # # overrides. You can do that directly here, just don't forget the
+        # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+        # # fonts?
+        # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
-    ];
+        # # You can also create simple shell scripts directly inside your
+        # # configuration. For example, this adds a command 'my-hello' to your
+        # # environment:
+        # (pkgs.writeShellScriptBin "my-hello" ''
+        #   echo "Hello, ${config.home.username}!"
+        # '')
+      ];
 
-    # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
-    home.file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
+      # Home Manager is pretty good at managing dotfiles. The primary way to manage
+      # plain files is through 'home.file'.
+      file = {
+        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+        # # symlink to the Nix store copy.
+        # ".screenrc".source = dotfiles/screenrc;
 
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
+        # # You can also set the file content immediately.
+        # ".gradle/gradle.properties".text = ''
+        #   org.gradle.console=verbose
+        #   org.gradle.daemon.idletimeout=3600000
+        # '';
+      };
+      sessionVariables = {
+        SHELL = "${pkgs.zsh}/bin/zsh";
+      };
+      shellAliases = {
+        ls = "eza";
+        x = "trash";
+      };
+
+      # Home Manager can also manage your environment variables through
+      # 'home.sessionVariables'. These will be explicitly sourced when using a
+      # shell provided by Home Manager. If you don't want to manage your shell
+      # through Home Manager then you have to manually source 'hm-session-vars.sh'
+      # located at either
+      #
+      #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+      #
+      # or
+      #
+      #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+      #
+      # or
+      #
+      #  /etc/profiles/per-user/eugene/etc/profile.d/hm-session-vars.sh
+      #
+      sessionVariables = {
+        # EDITOR = "emacs";
+      };
     };
 
-    home.sessionVariables = {
-      SHELL = "${pkgs.zsh}/bin/zsh";
-    };
-    home.shellAliases = {
-      ls = "eza";
-      x = "trash";
-    };
     programs = {
+      tealdeer = {
+        enable = true;
+      };
       zoxide = {
         enable = true;
       };
@@ -266,26 +291,6 @@ in
         enable = true;
         terminal = "${pkgs.alacritty}/bin/alacritty";
       };
-    };
-
-    # Home Manager can also manage your environment variables through
-    # 'home.sessionVariables'. These will be explicitly sourced when using a
-    # shell provided by Home Manager. If you don't want to manage your shell
-    # through Home Manager then you have to manually source 'hm-session-vars.sh'
-    # located at either
-    #
-    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  /etc/profiles/per-user/eugene/etc/profile.d/hm-session-vars.sh
-    #
-    home.sessionVariables = {
-      # EDITOR = "emacs";
     };
     xsession.windowManager.i3 = {
       enable = true;
