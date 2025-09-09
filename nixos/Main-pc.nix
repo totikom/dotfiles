7a036b82d91e4cfa2af5d3fbac06c9cdfda84b1c
Eugene Lomov <eugene.lomov@protonmail.com>
2025-09-09 21:59:21 +0300

    Configure syncthing for Main-pc

--- nixos/Main-pc.nix
+++ nixos/Main-pc.nix
@@ -7,6 +7,65 @@
 
 {
   services = {
+    syncthing = {
+      enable = true;
+      openDefaultPorts = true;
+      overrideFolders = true;
+      user = "eugene";
+      dataDir = "/home/eugene";
+      overrideDevices = true;
+      cert = config.sops.secrets."syncthing/Main-pc/cert.pem".path;
+      key = config.sops.secrets."syncthing/Main-pc/key.pem".path;
+      extraFlags = [ "--no-default-folder" ]; # Don't create default ~/Sync folder
+      settings = {
+        devices = {
+          "Redmi K80 Pro" = {
+            id = "7KDCBRR-3VGLY3C-QZ2T6TL-2FC76U6-3AEEEXQ-JHTYWCP-D6LFWBU-7OTWCAR";
+          };
+          "ThinkPad" = {
+            id = "I3NH3E2-RTOWUQI-RYIRCVC-WOHX2GJ-V6TOFTD-BGOWB22-B5E67VQ-77DHHQA";
+          };
+          "ThinkPadT490s" = {
+            id = "FMJ3P2U-2FEYA7S-TGSEAU5-EKLDGYS-RJ3VJ2O-RLMUM27-OJYDAYK-QF5WNQG";
+          };
+        };
+        folders = {
+          "tab.digital" = {
+            label = "tab.digital";
+            id = "ego4c-ckkzv";
+            path = "~/Documents/tab.digital";
+            devices = [
+              "Redmi K80 Pro"
+              "ThinkPad"
+              "ThinkPadT490s"
+            ];
+          };
+          "Documents" = {
+            id = "fp5rw-7j1x3";
+            path = "~/Documents/Phone";
+            devices = [
+              "Redmi K80 Pro"
+              "ThinkPad"
+              "ThinkPadT490s"
+            ];
+          };
+          "Phone Photos" = {
+            id = "0yno0-m0zuz";
+            path = "~/Pictures/Phone";
+            devices = [
+              "Redmi K80 Pro"
+            ];
+          };
+          "Music" = {
+            id = "bxcjr-59xqc";
+            path = "~/Music";
+            devices = [
+              "Redmi K80 Pro"
+            ];
+          };
+        };
+      };
+    };
     yggdrasil = {
       enable = true;
       configFile = config.sops.secrets."yggdrasil/Main-pc/conf".path;
@@ -22,8 +81,8 @@
         ];
       };
     };
-
   };
+
   sops =
     let
       wifi_generator = name: value: {
@@ -34,6 +93,18 @@
       };
       generated_secrets = builtins.mapAttrs (wifi_generator) (builtins.readDir (../wifis));
       hand_written_secrets = {
+        "syncthing/Main-pc/cert.pem" = {
+          format = "binary";
+          owner = config.users.users.eugene.name;
+          mode = "0600";
+          sopsFile = ../syncthing/Main-pc/cert.pem;
+        };
+        "syncthing/Main-pc/key.pem" = {
+          format = "binary";
+          owner = config.users.users.eugene.name;
+          mode = "0600";
+          sopsFile = ../syncthing/Main-pc/key.pem;
+        };
         "yggdrasil/Main-pc/conf" = {
           format = "binary";
           sopsFile = ../yggdrasil/Main-pc/yggdrasil.conf;
