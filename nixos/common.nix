9a0da56fba5dcd1c5b05655f711af78a037ddad8
Eugene Lomov <eugene.lomov@protonmail.com>
2025-09-17 21:34:43 +0300

    Switch from bees to duperemove

--- nixos/common.nix
+++ nixos/common.nix
@@ -149,6 +149,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     htop
+    duperemove
   ];
 
   programs = {
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -42,21 +42,6 @@
     fileSystems = [ "/" ];
   };
 
-  services.beesd.filesystems = {
-    root = {
-      spec = "/";
-      hashTableSizeMB = 1024;
-      verbosity = "info";
-      extraOptions = [
-        "--loadavg-target"
-        "1.0"
-      ];
-    };
-  };
-
-  # Disable beesd@root by default
-  systemd.services."beesd@root".wantedBy = lib.mkForce [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
